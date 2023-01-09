3de8d2726ab2210ecafbe131401c8eee4c25b499
Vagisha Nidhi <vagishn@amazon.com>
2023-01-09 16:51:18 +0000

    initial commit

---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebView2_UWP.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private Uri source;
+        public Uri Source
+        {
+            get { return source; }
+            set
+            {
+                source = value;
+                //RaisePropertyChanged();
+            }
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WebView2_UWP.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        /// <summary>
+        /// Fires an event when called. Used to update the UI in the MVVM world.
+        /// [CallerMemberName] Ensures only the peoperty that calls it gets the event
+        /// and not every property
+        /// </summary>
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
